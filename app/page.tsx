1dbcfa2a747f0f917413cfa90e77f19e5477866e
twentyOne2x <contact@attn.market>
2025-10-12 01:17:19 +0200

    feat: remove `home` wording

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,10 @@ import ShareChatHeader from '@/components/share-chat-header';
 import { auth } from '@/auth';
 
 export const metadata: Metadata = {
-  title: 'Home - icm.fyi ICM Research Chatbot',
+  title: 'icm.fyi ICM Research Chatbot',
   description: 'Interact with the icm.fyi ICM chatbot to explore Internet Capital Markets (ICM) insights.',
   openGraph: {
-    title: 'Home - icm.fyi ICM Research Chatbot',
+    title: 'icm.fyi ICM Research Chatbot',
     description: 'Interact with the icm.fyi ICM chatbot to explore Internet Capital Markets (ICM) insights.',
     url: `${process.env.NEXTAUTH_URL || 'http://localhost:3000'}/`,
     images: ['/opengraph-image.png'],
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: 'summary_large_image',
-    title: 'Home - icm.fyi ICM Research Chatbot',
+    title: 'icm.fyi ICM Research Chatbot',
     description: 'Interact with the icm.fyi ICM chatbot to explore Internet Capital Markets (ICM) insights.',
     images: ['/twitter-image.png'],
   },
